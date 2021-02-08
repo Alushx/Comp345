e745237cd03362074edee65fccc90079eeee939c
Sobhan-M <72103946+Sobhan-M@users.noreply.github.com>
2021-02-08 16:13:37 -0500

    Created Territory Class For Maps

--- Map.cpp
+++ Map.cpp
@@ -23,3 +23,35 @@ using namespace std;
 		}
 	}
 
+	// Territory default constructor.
+	Territory::Territory()
+	{
+		numOfArmies = 0;
+		armies = std::list<Army*>();
+	}
+	// Territory destructor.
+	Territory::~Territory()
+	{
+		// Delete each pointer inside the list to avoid memory leaks.
+		for (std::list<Army*>::iterator iter = armies.begin(); iter != armies.end(); iter++)
+		{
+			delete *iter;
+			*iter = nullptr;
+		}
+	}
+	void Territory::RemoveArmy(Army* army)
+	{
+		numOfArmies--;
+		armies.remove(army); // Does this cause memory leak?
+	}
+	void Territory::AddArmy(Army* army)
+	{
+		numOfArmies++;
+		armies.push_back(army);
+	}
+	std::string Territory::Owner()
+	{
+		// Count how many armies belong to each player.
+		// Player with the most armies has their name returned.
+	}
+
--- Map.h
+++ Map.h
@@ -13,3 +13,17 @@ public:
 	void addEdge(int, int);
 	void printList();
 };
+
+class Territory
+{
+private:
+	int numOfArmies;
+	std::list<Army*> armies;
+	
+public:
+	Territory();
+	~Territory();
+	void RemoveArmy(Army*);
+	void AddArmy(Army*);
+	std::string Owner();
+};
