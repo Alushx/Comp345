31b230685618ff42a9e433f7b0492cde5bad5249
Alushx <72513274+Alushx@users.noreply.github.com>
2021-02-09 18:56:37 -0500

    Update MapDriver.cpp

--- MapDriver.cpp
+++ MapDriver.cpp
@@ -2,14 +2,44 @@
 #include "Map.h"
 
 
-
 int main() {
 
-	Map m1(3);
-	m1.addEdge(0, 1);
-	m1.addEdge(1, 2);
+	Continent A(7);
+	A.addEdge(0, 1);
+	A.addEdge(1, 2);
+	A.addEdge(2, 3);
+	A.addEdge(2, 4);
+	A.addEdge(2, 5);
+	A.addEdge(5, 6);
 
-	m1.printList();
+	cout << "Continent A:\n";
 
-	return 0;
+	A.printList();
+
+	Continent B(5);
+	B.addEdge(0, 1);
+	B.addEdge(1, 2);
+	B.addEdge(1, 3);
+	B.addEdge(2, 3);
+	B.addEdge(2, 4);
+
+	cout << "\nContinent B:\n";
+
+	B.printList();
+
+	Continent C(3);
+	C.addEdge(0, 1);
+	C.addEdge(1, 2);
+
+	cout << "\nContinent C:\n";
+
+	C.printList();
+
+	Continent D(3);
+	D.addEdge(0, 1);
+	D.addEdge(1, 2);
+
+	cout << "\nContinent D:\n";
+
+	D.printList();
 }
