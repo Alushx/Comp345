9605655366a60f5c9405c0e96bf7c4edf5b85948
Alushx <72513274+Alushx@users.noreply.github.com>
2021-02-09 18:54:02 -0500

    Update Map.cpp

--- Map.cpp
+++ Map.cpp
@@ -2,17 +2,17 @@
 #include "Map.h"
 using namespace std;
 
-	Map::Map(int V) {
-		countries = V;
+	Continent::Continent(int V) {
+		this->countries = V;
 		countriesList = new list<int>[V];
 	}
 
-	void Map::addEdge(int x, int y) {
+	void Continent::addEdge(int x, int y) {
 		countriesList[x].push_back(y);
 		countriesList[y].push_back(x);
 	}
 
-	void Map::printList() {
+	void Continent::printList() {
 		for (int i = 0; i < countries; i++)
 		{
 			cout << "Country " << i << "----";
