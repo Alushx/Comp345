12cd397408bacbe27c6739ad8a649fb323829545
Sobhan-M <72103946+Sobhan-M@users.noreply.github.com>
2021-02-13 20:08:39 -0500

    Added map methods

--- Map.h
+++ Map.h
@@ -2,27 +2,41 @@
 #include <list>
 #include <map>
 
+// Forward declaration to make compiler happy.
+class Territory;
+
 struct Map {
-	std::map<int, std::list< std::pair<int, int>> > countriesList;
+	std::map<Territory*, std::map<Territory*, int>> countriesList; // Adjacency matrix.
+	std::list<std::list<Territory*>> continentList;
+
 public:
 	Map();
-	void addEdge(int, int, int);
+	void addEdge(Territory*, Territory*, int);
 	void printList();
+	std::list<std::list<Territory*>>* GetContinents();
+	void AddContinent(std::list<Territory*>);
 	bool Validate();
 private:
-	void ValidateHelper(int node, std::map<int,bool>* visitedList);
+	bool IsConnectedMap();
+	void HelpVisitMap(Territory* node, std::map<Territory*,bool>* visitedList);
+	bool IsConnectedContinent(std::list<Territory*>*);
+	void HelpVisitContinent(Territory* node, std::map<Territory*, bool>* visitedList);
+	bool HasUniqueTerritories();
 };
 
 class Territory
 {
 private:
 	int numOfArmies;
+	std::string name;
 	
 public:
 	Territory();
+	Territory(std::string);
 	void RemoveArmy();
 	void AddArmy();
 	int GetNumOfArmies() const;
+	std::string GetName() const;
 
 // Operators.
 private:
--- MapDriver.cpp
+++ MapDriver.cpp
@@ -1,36 +1,58 @@
 #include <iostream>
+#include<map>
 #include "Map.h"
 
 int main() {
 
 	Map m1;
+	Territory city1("City 1");
+	Territory city2("City 2");
+	Territory city3("City 3");
+	Territory city4("City 4");
+	Territory city5("City 5");
+	Territory city6("City 6");
+	Territory city7("City 7");
+	Territory city8("City 8");
+	Territory city9("City 9");
+	Territory city10("City 10");
+	Territory city11("City 11");
+	Territory city12("City 12");
+	Territory city13("City 13");
+	Territory city14("City 14");
+	Territory city15("City 15");
+	Territory city16("City 16");
+	Territory city17("City 17");
+	Territory city18("City 18");
+	Territory city19("City 19");
+	Territory city20("City 20");
 
 	// Continent A
-	m1.addEdge(1, 2, 1);
-	m1.addEdge(2, 3, 1);
-	m1.addEdge(3, 4, 1);
-	m1.addEdge(3, 5, 1);
-	m1.addEdge(3, 6, 1);
-	m1.addEdge(6, 7, 1);
-	m1.addEdge(6, 8, 3);
+	m1.addEdge(&city1, &city2, 1);
+	m1.addEdge(&city2, &city3, 1);
+	m1.addEdge(&city3, &city4, 1);
+	m1.addEdge(&city3, &city5, 1);
+	m1.addEdge(&city3, &city6, 1);
+	m1.addEdge(&city6, &city7, 1);
+	m1.addEdge(&city6, &city8, 3);
 
 	// Continent B
-	m1.addEdge(8, 9, 1);
-	m1.addEdge(8, 13, 1);
-	m1.addEdge(8, 14, 1);
-	m1.addEdge(14, 15, 1);
-	m1.addEdge(9, 10, 3);
-	m1.addEdge(14, 16, 3);
+	m1.addEdge(&city8, &city9, 1);
+	m1.addEdge(&city8, &city13, 1);
+	m1.addEdge(&city8, &city14, 1);
+	m1.addEdge(&city14, &city15, 1);
+	m1.addEdge(&city9, &city10, 3);
+	m1.addEdge(&city14, &city16, 3);
 
 	// Continent C
-	m1.addEdge(10, 11, 1);
-	m1.addEdge(11, 12, 1);
+	m1.addEdge(&city10, &city11, 1);
+	m1.addEdge(&city11, &city12, 1);
 
 	// Continent D
-	m1.addEdge(16, 17, 1);
-	m1.addEdge(17, 18, 1);
+	m1.addEdge(&city16, &city17, 1);
+	m1.addEdge(&city17, &city18, 1);
 
 	m1.printList();
+	m1.Validate();
 
 	return 0;
 }
