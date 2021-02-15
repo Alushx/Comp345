e20fdb586c2f10aef54c49cc1cbed4e0de71db98
carlt14 <carltuquero14@gmail.com>
2021-02-14 19:21:59 -0500

    Update Cards.h
    Made variables private

--- Cards/Cards.h
+++ Cards/Cards.h
@@ -8,36 +8,46 @@ using namespace std;
 
 
 class Card {
-public:
+private:
 	string good;
 	string combinationtype;
 	string action[2];
 
+public:
 	//Constructor
-	Card()= default;
+	Card()=default;
 	Card(string good, string combinationtype, string action);
 	Card(string good, string combinationType, string firstAction, string secondAction);
+	
+	string getGood();
+	string getCombinationType();
+	string getFirstAction();
+	string getSecondAction();
 
 	//display card
 	void printCard();
 };
 
 class Deck {
-public:
+private:
 	Card *topCardptr;
-	Card cards[43];
+	Card cards[34];
+public:
 	//Construction
 	Deck();
 	void generateDeck();
 	void printDeck();
 	void shuffleDeck();
 	Card *draw();
+private:
+	static int  myRandomGenerator(int j);
 };
 
 class Hand {
-public:
+private:
 	Deck* deck;
 	Card *cards[6];//Face-up cards
+public:
 	Hand(Deck* deck);
 	int getCardCost(int indexOfcard);
 	Card* exchange(int cardIndex, int* playerCoins);
