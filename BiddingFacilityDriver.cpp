93224c90a3caf2c325833a059072be1d02c5d9a7
vithura14 <60198384+vithura14@users.noreply.github.com>
2021-02-16 10:19:25 -0500

    Update BiddingFacilityDriver.cpp

--- BiddingFacilityDriver.cpp
+++ BiddingFacilityDriver.cpp
@@ -3,35 +3,31 @@
 #include "Player.h"
 #include <vector>
 
-
-
 using namespace std;
+
 int main() {
 
-    // Local variables for testing
-    int numOfPlayers;
+    // Local variables
+    int numOfPlayers = 0;
     int bidWinner;
     string lName;
-    vector<Player*> player;
+    vector<Player*> player; //A vector of pointer-to-objects (which is Player)
     int coins = 0;
 
-    // Determine Number Of Players
+    //Prompt the user to enter a valid number Of players
     cout << "Welcome to Eight-Minute-Empire, how many Players are there?" << std::endl;
     cin >> numOfPlayers;
+    while (numOfPlayers < 2 || numOfPlayers > 4) {
+        std::cout << "The entered value is not valid! Please enter a number between 2 and 4 (inclusively)." << std::endl;
+        std::cin >> numOfPlayers;
+    } 
+    
+    //Determine number of coins allocated based on number of players
+    if (numOfPlayers == 4) coins = 9;
+    if (numOfPlayers == 3) coins = 11;
+    if (numOfPlayers == 2) coins = 14;
 
-    if (numOfPlayers == 4) {
-        coins = 9;
-    }
-
-    if (numOfPlayers == 3) {
-        coins = 11;
-    }
-
-    if (numOfPlayers == 2) {
-        coins = 14;
-    }
-
-    // Create all Players in Player vector
+    // Create all player objects in Player vector
     for (int i = 1; i <= numOfPlayers; i++) {
         cout << "Player " << i << " last name: " << std::endl;
         cin >> lName;
@@ -40,13 +36,8 @@ int main() {
         player.push_back(p);
     }
 
-    
-
     // Determine the first Player within the bidding phase
-    
-
     bidWinner = BiddingFacility::biddingPhase(player);
-     cout << "Index is: " << bidWinner;
 
     return 0;
 }
