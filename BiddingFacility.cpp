19b8580468863510cff97b22243d09733b937df3
vithura14 <60198384+vithura14@users.noreply.github.com>
2021-02-16 10:19:50 -0500

    Update BiddingFacility.cpp

--- BiddingFacility.cpp
+++ BiddingFacility.cpp
@@ -5,65 +5,77 @@
 
 using namespace std;
 
-// Constructor
+//Default Constructor
 BiddingFacility::BiddingFacility() {
     BiddingFacility::bid = new int(0);
 }
 
+//Copy constructor
+BiddingFacility::BiddingFacility(const BiddingFacility &bf) {
+    bid = bf.bid;
+}
 
-
-
-// Gameplay Methods
-
-
-// Returns the index of the player who goes first (ie. to determine player order)
+// Returns the index of the player who won the bid
 int BiddingFacility::biddingPhase(vector<Player*>& player) {
 
     // Local Variables
     int bid, highBid = 0, index = 0;
-    string winner="";
+    string bidWinner="";
+    bool zeroBid = false;
 
-    // Each player gets a chance to enter a bid
+    //Prompt each player to enter a valid bid value and set this value as that player's bid
     for (int i = 0; i < player.size(); i++) {
-        cout << "How much would you like to bid " << player[i]->getName() << "?" << std::endl;
+        cout << player[i]->GetName() << ", how much would you like to bid?" << std::endl;
         cin >> bid;
         while (bid < 0 || bid > player[i]->getCoins()) {
             std::cout << "This amount is not valid! Please enter a number between 0 and " << player[i]->getCoins() << ": " << std::endl;
             std::cin >> bid;
         }
-        cout << "Bid " << player[i]->getName() << "  " << bid << std::endl;
         player[i]->getBidFaci()->setBid(bid);
     }
 
+    cout << std::endl << "Bidding result is: " << std::endl;
+
+    //Compare a player's bid vale with the next player's bid value and determine who is the bid winner
     for (int i = 0; i < player.size(); i++) {
         if (player[i]->getBidFaci()->getBid() > highBid) {
             highBid = player[i]->getBidFaci()->getBid();
-            winner = player[i]->getName();
+            bidWinner = player[i]->GetName();
             index = i;
         }
         if (i < (player.size() - 1)) {
+            //If a player's bid is equal to the next player's bid (can also be 0), then determine the player whose alphabetical last name order wins
             if (player[i + 1]->getBidFaci()->getBid() == highBid) {
-                for (int j = 0; j < player[i]->getName().length(); ++j) {
-                    if (winner[j] > player[i + 1]->getName()[j]) {
-                        winner = player[i + 1]->getName();
+                for (int j = 0; j < player[i]->GetName().length(); ++j) {
+                    if (player[i]->GetName()[j] > player[i + 1]->GetName()[j]) {
+                        bidWinner = player[i + 1]->GetName();
                         highBid = player[i + 1]->getBidFaci()->getBid();
                         index = i+1;
                         break;
                     }
-                    if (winner[j] < player[i + 1]->getName()[j]) break;
+                    if (player[i]->GetName()[j] < player[i + 1]->GetName()[j]) {
+                        bidWinner = player[i]->GetName();
+                        highBid = player[i]->getBidFaci()->getBid();
+                        index = i;
+                        break;
+                    }
                 }
             }
         }
+        //Display each player's bid value at the same time
+        cout << player[i]->GetName() << " bids " << player[i]->getBidFaci()->getBid() << std::endl;
     }
-    cout << "Winner is: " << winner << " with highest bid of: " << highBid << std::endl;
+    //Display the bid winner
+    cout << "The bid winner is: " << bidWinner << " with a bid value of " << highBid << std::endl;
     return index;
 }  
 
+//Getting the bid value
 int BiddingFacility::getBid() const {
     return *bid;
 }
 
-
+//Setting the bid value
 void BiddingFacility::setBid(int bid) {
     *BiddingFacility::bid = bid;
 }
