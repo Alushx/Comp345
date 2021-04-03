062b5c94cc57c0bb4a4efcbdff7f6ceb9a8e5b3f
carlt14 <carltuquero14@gmail.com>
2021-04-03 15:04:05 -0400

    Create PlayerStrategies.h

---
+++ PlayerStrategies.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string.h>
+#include "Cards/Cards.h"
+using namespace std;
+//Abstract Class
+class PlayerStrategies{
+public:
+    virtual int pickCard(Hand *hand) = 0;
+    virtual string displayCurrentStrategy() = 0;
+};
+
+//Human player: requires user interaction to make decisions
+class HumanPlayer : public PlayerStrategies{
+    public:
+    virtual int pickCard(Hand *hand);
+    virtual string displayCurrentStrategy();
+};
+
+//Greedy Computer: player that focuses on building cities or destroying opponents
+class GreedyComputer : public PlayerStrategies{
+ public:
+    virtual int pickCard(Hand *hand);
+    virtual string displayCurrentStrategy();
+};
+
+//Moderate computer: control a region in which it just needs to occupy it with more armies than the opponents
+class ModerateComputer : public PlayerStrategies{
+ public:
+    virtual int pickCard(Hand *hand);
+    virtual string displayCurrentStrategy();
+}; 
