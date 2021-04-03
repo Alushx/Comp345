e47c084d2be27203a9cfbba19ec8173bb2917951
Alushx <72513274+Alushx@users.noreply.github.com>
2021-04-03 15:51:30 -0400

    Update PlayerStrategies.cpp
    Added abilities for extra move/add armies

--- PlayerStrategies.cpp
+++ PlayerStrategies.cpp
@@ -26,6 +26,7 @@ int GreedyComputer::pickCard(Hand *hand){
     {
         if(handcards[i].getFirstAction() == act.DESTROY_1_ARMY || handcards[i].getFirstAction() == act.BUILD_CITY ||
         handcards[i]->getSecondAction() == act.DESTROY_1_ARMY || handcards[i]->getSecondAction() == act.BUILD_CITY){
+        handcards[i].getSecondAction() == act.DESTROY_1_ARMY || handcards[i].getSecondAction() == act.BUILD_CITY){
            return i;
         }
     }
@@ -40,13 +41,16 @@ string GreedyComputer::displayCurrentStrategy() {
 int ModerateComputer::pickCard(Hand *hand){
     int cheapestCard = 0;
     Action act;
+    Ability ability;
     Card *handcards = hand->getHandCards();
     //go through the hand
     //if current card's action(s) have move or add army
     for (int i = 0; i < 6; i++)
     {
         if(handcards[i].getFirstAction().find("Add") !=std::string::npos || handcards[i].getFirstAction().find("Move") !=std::string::npos
-        || handcards[i].getSecondAction().find("Add") !=std::string::npos || handcards[i].getSecondAction().find("Move") !=std::string::npos)
+        || handcards[i].getSecondAction().find("Add") !=std::string::npos || handcards[i].getSecondAction().find("Move") !=std::string::npos
+        || handcards[i].getAbility() == ability.PLUS_ONE_MOVE || handcards[i].getAbility() == ability.PLUS_ONE_ARMY ||
+            handcards[i].getAbility() == ability.IMMUNE_TO_ATTACK || handcards[i].getAbility() == ability.FYLING)
         {
             return i;
         }
