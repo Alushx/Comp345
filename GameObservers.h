8d163387a424f2d7ff7dfdbab34d8ff24b9106de
Adam Yafout <adam.yafout@gmail.com>
2021-04-03 13:45:46 -0400

    Observer Subject Classes added

---
+++ GameObservers.h
@@ -0,0 +1,37 @@
+//===================================================================
+// Course: COMP 345
+// Professor: Nora Houari
+// Team: 14
+// Students:
+//      Adam Yafout - 40040306
+//      Bryan Lee - 40079332
+//      Carl Randyl Tuquero - 40067781
+//      Sobhan Mehrpour - 40122438
+//      Vithura Muthiah - 40062305
+//===================================================================
+
+#pragma once
+#include <list>
+using namespace std;
+
+class Observer
+{
+public:
+	~Observer();
+	virtual void Update() = 0;
+protected:
+	Observer();
+};
+
+// the subject Class
+class Subject {
+public:
+	virtual void Attach(Observer* o);
+	virtual void Detach(Observer* o);
+	virtual void Notify();
+	Subject();
+	~Subject();
+private:
+	list<Observer*>* _observers;
+
+};
